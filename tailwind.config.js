fe2d996eec559b4ca474a1a8ee459e601404380b
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-08-29 14:26:26 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neutralSilver': '#F5F7FA',
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#4682b4',
+        'neutralGary': '#71717171',
+      }
+    },
+  },
+  plugins: [
+    flowbite.plugin(),
+
+  ],
+}
+
